4ae49221dcf2308b95b1490da5945b411a0da705
wrapdavid <etdog0910@gamil.com>
2020-02-11 09:54:09 +0800

    practices 5.6 -Page178\

--- src/Main35.cpp
+++ src/Main35.cpp
@@ -1,8 +1,26 @@
 #include<iostream>
-#include<string>
 #include<vector>
 int main() {
+	const std::vector<std::string> scores = {"F", "D", "C", "B", "A", "A++" };
+	int grade = 50;
+	std::string lettergrade;
+	for (; std::cin >> grade && grade != 101;) {
+
+	if (grade < 60)
+	{
+		lettergrade = scores[0];
+	}
+	else
+	{
+		lettergrade = scores[(grade - 50 )/ 10];
+		if (grade != 100)
+		{
+			lettergrade += (grade % 10) > 7 ? "+" : grade % 10 < 3 ? "-" : "";
+		}
+	}
+	std::cout << lettergrade << std::endl;
+	}
+
 	
 
-	std::cin.get();
 }
