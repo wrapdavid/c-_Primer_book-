84a8cb594cb72a1c21595e553090833fd1e939a1
wrapdavid <etdog0910@gmail.com>
2020-03-14 00:56:06 +0800

    practice  7.1 -Page256

--- src/Main59.cpp
+++ src/Main59.cpp
@@ -1,5 +1,42 @@
 #include<iostream>
+#include<fstream>
+struct Sale_data
+{
+	std::string bookNo;
+	unsigned units_sold = 0;
+	double revenue = 0.0;
+
+};
+
 int main() {
-	std::cin.get();//換電腦最後測試，看放上去，time graph有否成功.
+	Sale_data total;
+	if (std::cin >> total.bookNo >> total.units_sold >> total.revenue) {
+		Sale_data trans;
+		while (std::cin >> trans.bookNo >> trans.units_sold >> trans.revenue)
+		{
+			if (total.bookNo == trans.bookNo) {
+				total.units_sold += trans.units_sold;
+				total.revenue += trans.revenue;
+			}
+			else
+			{
+				std::cout << total.bookNo << " " << total.units_sold << " "
+					<< total.revenue << std::endl;
+				total = trans;
+
+			}
+		}
+		std::cout << total.bookNo << " " << total.units_sold << " "
+			<< total.revenue << std::endl;
+	}
+	else
+	{
+		std::cout << "no data?" << std::endl;
+		return -1;
+	}
+
+
+
+
 
 }
